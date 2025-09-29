9e29fa42de8c9f6b1e64e8aa6e182b44a8bb0360
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-09-29 14:29:59 +0530

    models

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+import bcrypt
+
+
+db = SQLAlchemy()
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
