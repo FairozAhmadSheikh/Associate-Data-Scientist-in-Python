e761a7ac32bc3f02f5fcceee5651aabb0b4f36c1
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-09-29 14:32:21 +0530

    login manager

--- app.py
+++ app.py
@@ -15,3 +15,7 @@ db.init_app(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
