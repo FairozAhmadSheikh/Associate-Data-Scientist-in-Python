63704ce06b46f967d0985761b06756882e003f68
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-09-29 14:31:59 +0530

    extension

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ from flask_migrate import Migrate
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 import uuid
 from tasks import run_demo_port_scan
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+# extensions
+db.init_app(app)
+migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
