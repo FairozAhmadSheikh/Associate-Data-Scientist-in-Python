4c0f37fb2e2ebdbffb4cd7c639392f80caa9a55a
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-09-29 14:31:35 +0530

    app main imports done

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from config import Config
+from models import db, User, ScanJob
+from flask_migrate import Migrate
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+import uuid
+from tasks import run_demo_port_scan
