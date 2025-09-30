86c6ca6781af302f2765d1ee248c4c046c2fd52f
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-09-30 22:47:23 +0530

    optionally configure celery

--- celery_worker.py
+++ celery_worker.py
@@ -3,3 +3,9 @@ from config import Config
 
 
 celery = Celery(__name__, broker=Config.REDIS_URL, backend=Config.REDIS_URL)
+
+
+# optionally configure celery can be done fRom here
+celery.conf.task_serializer = 'json'
+celery.conf.result_serializer = 'json'
+celery.conf.accept_content = ['json']
